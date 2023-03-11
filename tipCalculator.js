cc4321b5456a4ce697151f476b5c64f1b28889bd
Adnan_shaikh <adnan.sits.mech@gmail.com>
2023-03-11 17:00:51 +0530

    second commit

--- tipCalculator.js
+++
@@ -1,7 +0,0 @@
-
-function calculateTotal() {
-  let bill = Number(document.getElementById("bill").value);
-  let tipPercentage = Number(document.getElementById("tip").value);
-  let total = bill + tipPercentage;
-  document.getElementById("total").innerHTML = `Total: ${total}`;
-}
